a9cf8d289b2953d42692a529ff3ce21e03a2e8d2
Thorinwasher <ragnarlol96@gmail.com>
2021-03-08 22:22:48 +0100

    Update phantom.py

--- phantom.py
+++ phantom.py
@@ -5,4 +5,49 @@
  
  @author: Thorin
 """
+import socket
+import struct
+import json
+import time
+
+JSON_Response = {
+    "version": {
+        "name": "1.16.5",
+        "protocol": 754
+    },
+    "players": {
+        "max": 0,
+        "online": 0,
+        "sample": []
+    },
+    "description": {
+        "text": "foo",
+        "bold": "true",
+        "extra": [
+        {"text": "bar"},
+        
+        {"text": "baz",
+         "bold": "false"},
+        
+        {"text": "qux",
+         "bold": "true"}
+        ]
+    },
+    "favicon": "data:image/png;base64,<data>"
+}
+
+class StatusPing:
+    
+    def __init__(self, host='localhost', port=25565, timeout=5):
+        """ Init the hostname and the port """
+        self._host = host
+        self._port = port
+        self._timeout = timeout
+        
+        
+    
+    
+    
+
+
 
