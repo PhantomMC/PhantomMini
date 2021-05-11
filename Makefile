3bfeab15ea0d2d26b6f43a05ae4f9358495c619f
Pheotis <49039536+Pheotis@users.noreply.github.com>
2021-05-11 18:53:26 -0400

    Added basic, prelim structure.
    Subject to change.

---
+++ Makefile
@@ -0,0 +1,5 @@
+# Temporary makefile
+# Should ideally be kept up to date by an ide
+
+PhantomServer: phantom.c
+	gcc -o PhantomServer phantom.c
---
+++ phantom.c
@@ -0,0 +1,17 @@
+/*
+ LCLO Phantom Server (C Edition)
+
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ 
+ @author: Thorin
+*/
+
+#include <stdio.h>
+#include "./lib/data/config_mngr.h"
+
+int main(int argc, char argv[]) {
+	printf("hello world\n");
+	return 0;
+}
